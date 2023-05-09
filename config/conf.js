ca5e0f58c41168473282239542b81d3a87bea1ca
Jaticindy <112738817+Jaticindy@users.noreply.github.com>
2023-05-09 11:05:34 +0700

    Update conf.js

--- config/conf.js
+++ config/conf.js
@@ -2,7 +2,7 @@ const mysql = require ('mysql')
 
 const db= mysql.createConnection({
     host:process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
+    user: usmtaxyvunyixctf,
     password:process.env.DB_PASS,
     database: process.env.DB_DATABASE
 })
