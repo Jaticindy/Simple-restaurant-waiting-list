526bb57ad66d7979ea81068ac81b8af66be7456e
jaticindy~ <jati.cindy@gmail.com>
2023-05-10 12:07:12 +0700

    setting konfigurasi

--- config/conf.js
+++ config/conf.js
@@ -1,18 +1,43 @@
-const mysql = require ('mysql')
+// const mysql = require ('mysql')
 
-const db= mysql.createConnection({
-    host:process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password:process.env.DB_PASS,
-    database: process.env.DB_DATABASE
-})
+// const db= mysql.createConnection({
+    
+//     host: "containers-us-west-66.railway.app",
+//     user: "root",
+//     password: "5ExjkHXc60NHg72tngP6",
+//     database: "railway"
+// })
+// // process.env.DB_DATABASE
 
-db.connect((err) => {
-    if(err) {
-        console.log('Koneksi gagal:', err);
-    } else {
-        console.log('Koneksi berhasil!');
-    }
+// db.connect((err) => {
+//     if(err) {
+//         console.log('Koneksi gagal:', err);
+//     } else {
+//         console.log('Koneksi berhasil!');
+//     }
+// });
+
+// module.exports=db
+
+
+var mysql = require('mysql');
+
+var db = mysql.createConnection({
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
 });
 
-module.exports=db
+db.connect((err) => {
+  if (err) {
+    console.log('Koneksi gagal:', err);
+  } else {
+    console.log('Koneksi berhasil!');
+  }
+});
+
+module.exports = db;
+
+
