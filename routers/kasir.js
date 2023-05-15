4741db411585a2d00db024e996be2bcf2d9fd0e3
jaticindy~ <jati.cindy@gmail.com>
2023-05-15 18:59:30 +0700

    basicAuth

--- routers/kasir.js
+++ routers/kasir.js
@@ -9,28 +9,28 @@ const { route } = require('./pesanan')
 
 
 // Get all meja
-router.get('/meja',KasirController.getMeja)
+router.get('/meja',auth,KasirController.getMeja)
 
 // Get all kasir
-router.get ('/',KasirController.getKasir)
+router.get ('/',auth,KasirController.getKasir)
   
 //get menu all
-router.get ('/menu',KasirController.getMenu)
+router.get ('/menu',auth,KasirController.getMenu)
 
  //get TotalHarga
- router.get ('/:nomor_meja/total_harga',KasirController.getTotalHarga)
+ router.get ('/:nomor_meja/total_harga',auth,KasirController.getTotalHarga)
 
 //Post Pembayaran
-router.post ('/:nomor_meja/pembayaran',KasirController.postPembayaran)
+router.post ('/:nomor_meja/pembayaran',auth,KasirController.postPembayaran)
 
 //Post Registrasi
-router.post('/registrasi',KasirController.postRegistrasi)
+router.post('/registrasi',auth,KasirController.postRegistrasi)
 
 //Put Registrasi
-router.put ('/edit/:id',KasirController.putRegistrasi)
+router.put ('/edit/:id',auth,KasirController.putRegistrasi)
   
 //Delete  
-router.delete('/delete/:id',KasirController.delID)  
+router.delete('/delete/:id',auth,KasirController.delID)  
  
   
 
